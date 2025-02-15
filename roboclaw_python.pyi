85a7ca1f9f28ecc2512b6c608b7390f7709dd6e8
kingananas20 <jonasarter@outlook.com>
2025-02-15 14:46:53 +0100

    added a working documentation to the python functions

---
+++ roboclaw_python.pyi
@@ -0,0 +1,36 @@
+from .roboclaw_python import *
+
+class Motor:
+    M1 = 1
+    M2 = 2
+
+class RoboClaw:    
+    def set_speed(self, motor: Motor, speed: int, address: int = None) -> bool: 
+        """
+        Sets the speed of a specified motor.
+
+        ### Detailed Description 
+        - motor: is either 1 or 2 
+        - speed: positive to make the motor turn into the positive direction and negative for the other way around
+        - address (optional): address of the roboclaw with the motor on (default to RoboClaw.new(address))
+        """
+    def drive(self, speed: int, address: int) -> bool:
+        """
+        Drives both motors in the same direction
+        
+        ### Detailed Description
+        - speed: negative to drive forward, positive to drive backwards
+        - address (optional): address of the roboclaw with the motors on (default to RoboClaw.new(address))
+        """
+    def turn(self, speed: int, address: int) -> bool:
+        """
+        Turn
+        
+        ### Detailed Description
+        - speed: negative to turn left, positive to turn right
+        - address (optional): address of the roboclaw with the motors on (default to RoboClaw.new(address))
+        """
+    def read_encoder(self, motor: Motor, address: int) -> int:
+        """
+        Reads and returns the encoder value of the specified motor
+        """
---
+++ test.py
@@ -0,0 +1,8 @@
+import roboclaw_python 
+
+roboclaw = roboclaw_python.RoboClaw()
+
+roboclaw.set_speed()
+roboclaw.drive()
+roboclaw.turn()
+roboclaw.read_encoder()
