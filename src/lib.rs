3bad8bf220cec2744c44dbb7f54add6e56425478
kingananas20 <jonasarter@outlook.com>
2025-04-13 12:39:37 +0200

    added allow deadcode

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 #[allow(unused_imports)]
-#[allow(dead_code)]
 
 mod roboclaw;
 use pyo3::prelude::*;
--- src/roboclaw/commands.rs
+++ src/roboclaw/commands.rs
@@ -1,3 +1,4 @@
+#[allow(dead_code)]
 #[derive(Debug, Clone, Copy)]
 pub enum Commands {
     M1Forward = 0,
